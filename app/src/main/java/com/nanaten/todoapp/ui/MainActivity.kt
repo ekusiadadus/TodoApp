9e95afa7fb2ded4d9f1b7e132a976eb7a06065f6
m.coder <kurokoshow00@gmail.com>
2020-02-02 16:50:39 +0900

    add Toolbar

--- app/src/main/java/com/nanaten/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/nanaten/todoapp/ui/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.nanaten.todoapp.ui
 
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.nanaten.todoapp.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Toolbarをセット
+        setSupportActionBar(toolbar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.todoListFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
