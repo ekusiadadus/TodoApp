a7b3546a863e141bb304c703ccb41f6e416dee68
m.coder <kurokoshow00@gmail.com>
2020-02-02 22:08:40 +0900

    add Realm init

--- app/src/main/java/com/nanaten/todoapp/App.kt
+++ app/src/main/java/com/nanaten/todoapp/App.kt
@@ -3,10 +3,23 @@ package com.nanaten.todoapp
 import com.nanaten.todoapp.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class App : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory()
             .create(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Realm
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
     }
 }
